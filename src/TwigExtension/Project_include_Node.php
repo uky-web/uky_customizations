c57d775e69e29b3152d6502ac9a3029057017a4c
GitHub <action@github.com>
2024-05-05 23:55:27 +0000

    Merge pull request #267 from uky-web/266-uky-customizations-include-compile-outputs-immediately

--- src/TwigExtension/Project_include_Node.php
+++ src/TwigExtension/Project_include_Node.php
@@ -6,47 +6,62 @@ use Twig\Compiler;
 use Twig\Node\IncludeNode;
 
 class Project_include_Node extends IncludeNode {
+
+
     public function compile(Compiler $compiler): void
     {
-        $expr = $this->getNode('expr');
+        $compiler->addDebugInfo($this);
 
+        //Specific to uky_customizations, get name for comments
+        $expr = $this->getNode('expr');
         $name = null;
         if ($expr->hasAttribute('value')) {
             $name = $expr->getAttribute('value');
         }
-
-        $compiler->addDebugInfo($this);
-
         if ($name) {
             $compiler->write("echo '<!-- TWIG INCLUDE : " . $name . "\" -->';\n");
         }
 
         if ($this->getAttribute('ignore_missing')) {
+            $template = $compiler->getVarName();
+
             $compiler
+                ->write(sprintf("$%s = null;\n", $template))
                 ->write("try {\n")
                 ->indent()
+                ->write(sprintf('$%s = ', $template))
             ;
-        }
 
-        $this->addGetTemplate($compiler);
+            $this->addGetTemplate($compiler);
 
-        $compiler->raw('->display(');
-
-        $this->addTemplateArguments($compiler);
-
-        $compiler->raw(");\n");
-
-        if ($this->getAttribute('ignore_missing')) {
             $compiler
+                ->raw(";\n")
                 ->outdent()
-                ->write("} catch (\\Twig\\Error\\LoaderError \$e) {\n")
+                ->write("} catch (LoaderError \$e) {\n")
                 ->indent()
                 ->write("// ignore missing template\n")
                 ->outdent()
-                ->write("}\n\n")
+                ->write("}\n")
+                ->write(sprintf("if ($%s) {\n", $template))
+                ->indent()
+                ->write(sprintf('yield from $%s->unwrap()->yield(', $template))
             ;
+
+            $this->addTemplateArguments($compiler);
+            $compiler
+                ->raw(");\n")
+                ->outdent()
+                ->write("}\n")
+            ;
+        } else {
+            $compiler->write('yield from ');
+            $this->addGetTemplate($compiler);
+            $compiler->raw('->unwrap()->yield(');
+            $this->addTemplateArguments($compiler);
+            $compiler->raw(");\n");
         }
 
+        //specific to uky_customizations
         if ($name) {
             $compiler->write("echo '<!-- END TWIG INCLUDE : " . $name . "\" -->';\n");
         }
